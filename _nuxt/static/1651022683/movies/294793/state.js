0ec5ff25b57d4a4b7cb836c8820ef260657bdba2
Bondok6 <kirolousbondok6@gmail.com>
2022-04-27 03:25:15 +0200

    Updates

---
+++ _nuxt/static/1651022683/movies/294793/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmovie-app\u002F_nuxt\u002Fstatic\u002F1651022683",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F294793",config:{_app:{basePath:"\u002Fmovie-app\u002F",assetsPath:"\u002Fmovie-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
