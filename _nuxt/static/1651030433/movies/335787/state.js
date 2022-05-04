af8edd490077dfc4bfc804d676d35173f5fc3613
Bondok6 <kirolousbondok6@gmail.com>
2022-05-04 04:03:32 +0200

    Updates

--- _nuxt/static/1651030433/movies/335787/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmovie-app\u002F_nuxt\u002Fstatic\u002F1651030433",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F335787",config:{_app:{basePath:"\u002Fmovie-app\u002F",assetsPath:"\u002Fmovie-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1651629479/movies/799876/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmovie-app\u002F_nuxt\u002Fstatic\u002F1651629479",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F799876",config:{_app:{basePath:"\u002Fmovie-app\u002F",assetsPath:"\u002Fmovie-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
